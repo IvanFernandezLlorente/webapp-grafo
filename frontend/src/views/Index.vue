e4e63819ee47e02b4591fc8e8e203dd55cdcab7a
IvanFernandezLlorente <48330943+IvanFernandezLlorente@users.noreply.github.com>
2020-12-06 12:16:04 +0100

    basic architecture

---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <Drawer />
+    <div class="main-panel">
+        <TopNavbar />
+        <Content />
+        <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  components: {
+    Drawer: () => import('./Drawer'),
+    Content: () => import('./Content'),
+    Footer: () => import('./Footer'),
+    TopNavbar: () => import('./TopNavbar')
+  }
+}
+</script>
+
+<style scoped>
+
+.wrapper {
+    position: relative;
+    top: 0;
+    height: 100vh;
+}
+
+.main-panel{
+    overflow: auto;
+    max-height: 100%;
+    height: 100%;
+    background: hsla(240,7%,81%,.15);
+    position: relative;
+    float: right;
+    width: calc(100% - 260px);
+    min-height: 100%;
+}
+</style>
