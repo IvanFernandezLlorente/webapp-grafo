76e9bc7f68c8511e08837a4b6ed50fad3ecfd92f
IvanFernandezLlorente <48330943+IvanFernandezLlorente@users.noreply.github.com>
2021-02-03 11:58:55 +0100

    Deleting files not used

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,11 +1,9 @@
 import userRoutes from './routes/user.routes';
 import publicationRoutes from './routes/publication.routes';
-import imagesRoutes from './routes/images.routes';
 import problemRoutes from './routes/problem.routes';
 
 export default app => {
     app.use('/api/users', userRoutes);
     app.use('/api/problems', problemRoutes);
     app.use('/api/publications', publicationRoutes);
-    app.use('/api/images', imagesRoutes);
 }
